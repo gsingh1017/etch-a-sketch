1686c71a02409364e384af10f2f2182d468ad9e8
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2024-11-11 14:40:45 -0800

    Add code to color grid squares
    Had to get help from google to get the querySelectorAll function to work correctly. Cleaned up code in script.js. Added button container in index.html. Added additional style in style.css.

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function createGrid(num) {
     // multiply num by itself to get correct number of squares in grid
     const gridSquareCount = num * num;
 
-    // calculate square size **CHECK**
+    // calculate square size (600px is max width/height)
     const squareSize = (600 / num);
 
-    const gridContainer = document.querySelector("#container");
+    const gridContainer = document.querySelector("#gridContainer");
 
-    // loop creates grid squares based on user input
+    // loop creates grid squares inside gridContainer based on user input
     for (let i = 0; i < gridSquareCount; i++) {
         const createGridSquare = document.createElement("div");
         createGridSquare.classList = "gridSquare";
 
-        // subtract by 1 to add 0.5px border to box
+        // subtract by 1 to add 0.5px border to grid square
         createGridSquare.style.height = (squareSize - 1) + "px";
         createGridSquare.style.width = (squareSize - 1) + "px";
 
@@ -25,6 +25,21 @@ function createGrid(num) {
 }
 
 
-// const gridSize = prompt("Pick a number for grid");
+function draw() {
+
+    // NodeList containing all grid squares
+    const changeSquareColor = document.querySelectorAll(".gridSquare");
+
+    // iterates through each element in the grid square node list
+    for (let elem of changeSquareColor) {
+
+        // if mouse hovers over element in the grid square NodeList, color background
+        elem.addEventListener("mouseenter", () => {
+            elem.style.backgroundColor = "green";
+        });
+    }
+}
+
 
 createGrid(16);
+draw();
